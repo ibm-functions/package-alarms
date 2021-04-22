f51064eb227f568a495a66f9837db615bfafe0b4
Nikolai Baudis <nikolai.baudis@ibm.com>
2021-04-22 17:06:45 +0200

    Bump node version to 12

--- installCatalog.sh
+++ installCatalog.sh
@@ -40,7 +40,7 @@ DB_NAME="${4}alarmservice"
 APIHOST="$5"
 WORKERS="$6"
 LIMIT_CRON_FIELDS="${LIMIT_CRON_FIELDS}"
-ACTION_RUNTIME_VERSION=${ACTION_RUNTIME_VERSION:="nodejs:10"}
+ACTION_RUNTIME_VERSION=${ACTION_RUNTIME_VERSION:="nodejs:12"}
 
 # If the auth key file exists, read the key in the file. Otherwise, take the
 # first argument as the key itself.
