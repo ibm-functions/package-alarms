9157ecd4a40eef11c266b14f47cb60b8cddaee80
GROEZING <groezing@de.ibm.com>
2023-09-28 15:56:04 +0200

    pattern test

--- provider/lib/manager.js
+++ provider/lib/manager.js
@@ -324,6 +324,8 @@ module.exports = function (logger, triggerDB, redisClient, databaseName) {
         try{
             logger.info(method, 'resetting system from last state');
             logger.info(method, 'loggertest1: ', '9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/_/alarms_worker0armada_host0_1695803541249');
+            logger.info(method, 'loggertest1a: ', '9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/_/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+            logger.info(method, 'loggertest1: ', '9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/alarms_worker0armada_host0_1695803541249');
             logger.info(method, "loggertest2: ", "9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/_/alarms_worker0armad");
             logger.info(method, "loggertest3a: ", "9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/_/alarms_worker1armada");
             logger.info(method, "loggertest3b: ", "9cfe57a0-7ac1-4bf4-9026-d7e9e591271f/_/alarms_worker0armada");
